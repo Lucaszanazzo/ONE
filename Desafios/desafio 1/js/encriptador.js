c78ab743155b54cb946899a1d90ef354d204f91e
Lucas Sebastian Zanazzo <lucaszanazzo4@gmail.com>
2022-06-14 12:09:58 -0300

    Comenzando el Challenge 1 de Alura, El encriptador

---
+++ Desafios/desafio 1/js/encriptador.js
@@ -0,0 +1,16 @@
+var botonEncriptar = document.querySelector("#encriptar");
+
+botonEncriptar.addEventListener("click",function(event){
+    event.preventDefault();
+
+    var textoForm = document.querySelector(".form");
+    var muñeco = document.querySelector("#muñeco"); 
+
+    textoForm.reset();
+})
+
+
+
+
+
+
